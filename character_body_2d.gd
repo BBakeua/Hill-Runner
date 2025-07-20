91b3ae343e5ebf0b304603041c559c35f595f565
BBakeua <bakbak67@kingshigh.school.nz>
2025-07-21 09:40:59 +1200

    Version testing 12435

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,36 +1,49 @@
 extends CharacterBody2D
 
+# Editable movement parameters
+@export var speed : float = 200.0
+@export var jump_velocity : float = -300.0
+@export var gravity : float = 980.0
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+# Animation node reference
 @onready var animated_sprite = $AnimatedSprite2D
 
+# State variables
+var is_jumping : bool = false
+
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("Move-left", "Move-right")
+	# Apply gravity
+	velocity.y += gravity * delta
 	
-	# Flip the sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+	# Horizontal movement
+	var input_direction = Input.get_axis("move_left", "move_right")
+	velocity.x = input_direction * speed
 	
-	# Apply movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		is_jumping = true
+	
+	# Update animation
+	update_animation(input_direction)
+	
+	# Move the character
 	move_and_slide()
+	
+	# Reset jump state if on floor
+	if is_on_floor():
+		is_jumping = false
+
+func update_animation(input_direction):
+	if not is_on_floor():
+		animated_sprite.play("Jump")
+	elif input_direction != 0:
+		animated_sprite.play("Run")
+		animated_sprite.flip_h = input_direction < 0
+	else:
+		animated_sprite.play("Idle")
+
+# Input map setup (set these in Project Settings > Input Map)
+# - "move_left": A/left arrow
+# - "move_right": D/right arrow
+# - "jump": Spacebar
